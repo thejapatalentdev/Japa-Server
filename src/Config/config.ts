0eb81c3ec5233306613e40cf310b682e1e131a27
temi724 <tayodaniel0@gmail.com>
2024-07-27 17:59:37 +0100

    On branch implementing_ts  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	new file:   src/Config/config.ts 	new file:   src/Config/connection.ts 	new file:   src/Controllers/japa.ts 	new file:   src/Models/admin.ts 	new file:   src/Models/jobs.ts 	new file:   src/Models/user.ts 	new file:   src/Routers/index.ts 	new file:   src/Routers/japa.ts 	deleted:    src/index.js 	new file:   src/index.ts 	new file:   tsconfig.json

---
+++ src/Config/config.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+  conn: process.env.CONN,
+  port: process.env.PORT,
+  //   mail: "",
+  //   pass: "",
+  //   infobip_key: process.env.INFOBIP_KEY,
+  //   info_bip_host: process.env.INFO_BIP_HOST,
+  //   key: process.env.KEY,
+  //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  //   region: process.env.AWS_REGION,
+  //   bucket: process.env.S3_BUCKET_NAME,
+  //   azure_storage_connection_string: "your_azure_storage_connection_string",
+  //   container_name: "your_azure_storage_container_name",
+};
+
+export default config;
